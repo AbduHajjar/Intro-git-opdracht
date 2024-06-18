9af65feec3718639be594ba40b5957d1ccd2e5ee
Abdu <ps251232@summacollege.nl>
2024-06-18 10:48:09 +0200

    Add project files.

---
+++ Intro-git-opdracht/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Intro_git_opdracht
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
